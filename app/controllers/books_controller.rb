e20a9bb52a398b559c9ac10de7b1bf587421965b
sora <sora621621@icloud.com>
2022-06-18 09:51:35 +0000

    提出

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+    
+  end
+    
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
+
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book.id)
+     flash[:notice] = "successfully" 
+    else
+     flash[:notice] = "error" 
+     @books = Book.all
+     render :edit
+    end
+  end
+  
+  def destroy
+   book = Book.find(params[:id])  
+   book.destroy
+   redirect_to books_path
+   flash[:notice] = "successfully" 
+  end
+  
+  private
+    
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
